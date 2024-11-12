76b6bd38ac14f72e21da33fe6cb66631c88103ff
Vaibhavi1112-code <vaibhavivpanchal11@gmail.com>
2024-11-12 01:47:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -427,7 +427,5 @@ def get_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
- 
-  
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
