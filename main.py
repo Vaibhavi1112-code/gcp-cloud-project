0ef1c3fd6a93e69f39806f36560ec469bc90aa9a
Vaibhavi1112-code <vaibhavivpanchal11@gmail.com>
2024-11-14 23:27:15 -0500

    Green

--- main.py
+++ main.py
@@ -245,7 +245,7 @@ def index():
                 <button class="logout-button">Logout</button>
             </a>
         </div>
-        <body style="background-color: rgb(51, 204, 254);">
+        <body style="background-color: rgb(10, 204, 153);">
         <form method="post" enctype="multipart/form-data" action="/upload">
             <div>
                 <label for="file">Choose file to upload</label>
@@ -369,7 +369,7 @@ def login_html():
         <h1>Login to Photo App</h1>
     </header>
     <main> 
-        <body style="background-color: rgb(51, 204, 254);">
+        <body style="background-color: rgb(10, 204, 153);">
         <h2>Please login to the Photo App</h2><br>
         <form method="post" action="/login">
             <div>
